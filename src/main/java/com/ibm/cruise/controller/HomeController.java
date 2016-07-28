bb4aa21df52eed6d98b02cd46230d0c504c7f989
Eric Hou <cruise_2013@aliyun.com>
2016-07-28 21:20:48 +0800

    update home controller

--- src/main/java/com/ibm/cruise/controller/HomeController.java
+++ src/main/java/com/ibm/cruise/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.ibm.cruise.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 	
 	@RequestMapping("/home")
-	public String home() {
+	public @ResponseBody String home() {
 		return "Hello Jenkins!";
 	}
 	
